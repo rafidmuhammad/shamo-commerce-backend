d3055bc9654385670a62b58a6c14c8b2b3d89a2b
Rafid Muhammad <rafidmuhammad@Rafids-MacBook-Pro.local>
2023-12-31 09:47:35 +0700

    add knex config files

---
+++ knexfile.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const environment = ['development', 'staging', 'production'];
+
+
+const connection = {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+};
+
+const commonConfig = {
+    client:'pg',
+    connection: connection,
+    migrations: {
+        tableName: 'knex_migrations',
+        directory: './migrations',
+    },
+    seeds: {
+        directory: './seeds',
+    },
+};
+
+
+module.exports = environment.map((env) => {return {environment:env, ...commonConfig}});
