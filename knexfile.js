80a2f056da2ffb7773f4b42b910e130345e6b708
Rafid Muhammad <rafidmuhammad@Rafids-MacBook-Pro.local>
2023-12-31 09:49:45 +0700

    add pool connection

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,10 @@ const connection = {
 const commonConfig = {
     client:'pg',
     connection: connection,
+    pool: {
+        min: 2,
+        max: 10,
+      },
     migrations: {
         tableName: 'knex_migrations',
         directory: './migrations',
