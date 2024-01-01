edcb82c84570812f3ba854de0d07cacb5f666dff
Rafid Muhammad <rafidmuhammad@Rafids-MacBook-Pro.local>
2024-01-01 16:24:02 +0700

    add product model

---
+++ src/models/productModel.js
@@ -0,0 +1,9 @@
+class Product {
+    constructor(name, price, description, tags, category) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.tags = tags;
+        this.category = category;
+    }
+}
