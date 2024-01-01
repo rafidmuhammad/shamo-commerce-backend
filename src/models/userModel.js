91c69d8a984611d671ee285e6c8ffe5973e78459
Rafid Muhammad <rafidmuhammad@Rafids-MacBook-Pro.local>
2024-01-01 14:44:20 +0700

    add user model

---
+++ src/models/userModel.js
@@ -0,0 +1,9 @@
+class UserModel{
+    constructor(username, fullname, email, password, roles) {
+        this.username = username;
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+}
