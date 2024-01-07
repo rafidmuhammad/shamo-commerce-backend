90b766fcbe9660a4517d34e9c16fbe426fb56868
Rafid Muhammad <rafidmuhammad@Rafids-MacBook-Pro.local>
2024-01-07 14:08:23 +0700

    add orm to product category model

--- src/models/productCategoryModel.js
+++ src/models/productCategoryModel.js
@@ -1,5 +1,26 @@
-class ProductCategory{
+const {knex, Model} = require('./index.js');
+
+
+class ProductCategory extends Model{
     constructor(productName) {
         this.productName = productName;
+    }
+
+    static get tableName(){
+        return 'product_categories';
+    }
+
+    static get relationMappings(){
+        const Product = require('./productModel');
+        return{
+            transactions: {
+                relation: Model.HasManyRelation,
+                modelClass: Product,
+                join: {
+                    from: 'product_categories.id',
+                    to: 'products.category_id'
+                }
+            },
+        }
     }
 }
