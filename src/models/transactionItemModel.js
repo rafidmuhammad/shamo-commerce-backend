d8df5e4c9b2910c3c34aad43cdef2d1e3771acb6
Rafid Muhammad <rafidmuhammad@Rafids-MacBook-Pro.local>
2024-01-07 14:54:40 +0700

    add orm for transaction item model

--- src/models/transactionItemModel.js
+++ src/models/transactionItemModel.js
@@ -1,3 +1,6 @@
+const {knex, Model} = require('./index.js');
+
+
 class TransactionItem {
     constructor(user_id, product_id, transaction_id, quantity) {
         this.user_id = user_id;
@@ -5,4 +8,25 @@ class TransactionItem {
         this.transaction_id = transaction_id;
         this.quantity = quantity;
     }
+
+
+    static get tableName(){
+        return 'transaction_items';
+    }
+
+    static get relationMappings(){
+        const Product = require('./productModel');
+        return{
+            product: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Product,
+                join: {
+                    from: 'transactions_item.product_id',
+                    to: 'products.id'
+                }
+            },
+        }
+    }
 }
+
+module.exports = {TransactionItem}
