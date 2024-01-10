61497ccc480e89614f5bbb0fea0a8fdc310193c8
Rafid Muhammad <rafidmuhammad@Rafids-MacBook-Pro.local>
2024-01-10 13:16:04 +0700

    update code structure and add muliple parameter filtering logic

--- src/routes/v1/productRoutes.js
+++ src/routes/v1/productRoutes.js
@@ -1,8 +1,97 @@
 const express = require('express');
 const router = express.Router();
+const ResponseFormat = require('../responseFormat');
+const {Product} = require('../../models/productModel');
 
-router.get('/', function(req, res) {
-    res.send('Hello from APIv1 root route.');
+router.get('/all', async function(req, res){
+    const id = req.query.id;
+    const name=req.query.name;
+    const limit=req.query.limit;
+    const tag=req.query.tag;
+    const category=req.query.category;
+    const priceFrom=req.query.priceFrom;
+    const priceTo = req.query.priceTo;
+
+
+    if(id){
+        try {
+            const product = await Product.query()
+                .withGraphFetched('[galleries, category]')
+                .findById(id);
+            let response;
+            if(product !== undefined){
+                response = {
+                    message : "Data successfully fetched",
+                    data: product
+                };
+            }
+            else{
+                response = {
+                    message : "Data not found",
+                    code : 404
+                };
+            }
+            res.json(response);
+          } 
+        catch (error) {
+            res.status(500).json({
+              message: 'Internal Server Error',
+            });
+          }
+    }
+    
+
+    try {
+        let productQuery = Product.query().withGraphFetched('[galleries, category]');
+        let productOut;
+        let response;
+
+
+        if(name){
+             productQuery = productQuery.whereRaw('LOWER(product_name) LIKE ?', [`%${name.toLowerCase()}%`]);
+        }
+
+        if(tag){
+            productQuery = productQuery.orWhereRaw('LOWER(tags) LIKE ?', [`%${tag.toLowerCase()}%`]);
+        }
+
+        if(priceFrom){
+            productQuery = productQuery.orWhere('product_price', '>=', priceFrom);
+        }
+
+        if(priceTo){
+            productQuery = productQuery.orWhere('product_price', '<=', priceTo);
+        }
+
+        if(category){
+            productQuery = productQuery.orWhere('category_id', category);
+        }
+
+        productOut = await productQuery.limit(limit ? limit : 10);
+        console.log(limit ? limit : 10);
+
+        if(productOut.length === 0){
+            response = {
+                message : "Data not found",
+                code : 404
+            };
+        }
+        else{
+            response = {
+                message : "Data successfully fetched",
+                data: productOut
+            };
+        }
+        res.json(response);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Internal Server Error',
+          });
+    }
+
+
+
   });
 
 module.exports = router;
