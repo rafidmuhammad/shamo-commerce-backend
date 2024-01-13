ec61b8c43a84e8d69c77aa702121afd21d185dc2
Rafid Muhammad <rafid.muhammad@gmail.com>
2024-01-14 06:12:44 +0700

    add user registration handler

--- src/routes/v1/userRoutes.js
+++ src/routes/v1/userRoutes.js
@@ -1,8 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const { checkUserWithEmail, hashPassword } = require('../../helper/userHelper');
+const ResponseFormat = require('../responseFormat');
+const Joi = require('joi');
+const { val } = require('objection');
+const User = require('../../models/userModel');
 
-router.get('/', function(req, res) {
-    res.send('Hello from APIv1 root route.');
-  });
+const registerSchema = Joi.object({
+  username: Joi.string().max(255).required(),
+  fullname: Joi.string().max(255).required(),
+  email_address: Joi.string().email().max(255).required(),
+  password: Joi.string().min(8).required()
+});
+
+async function validateUser(req, res, next) {
+  const { error, value } = registerSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
+  if (await checkUserWithEmail(value.email_address)) {
+    return res.status(400).json({ error: "Email is already in used" });
+  }
+  req.validatedData = value;
+  next();
+}
+
+
+router.post('/register', validateUser, async (req, res) => {
+
+  try {
+    const validatedUser = req.validatedData;
+
+    //stored hashed password
+    validatedUser.password = await hashPassword(validatedUser.password);
+
+    const inserted = await User.query().insert(validatedUser);
+
+    const response = ResponseFormat.success({ code: 201, messageText: "Created", dataOut: { username: inserted.username, email: inserted.email_address } });
+
+    return res.status(201).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Internal Server Error',
+    });
+  }
+}
+);
 
 module.exports = router;
