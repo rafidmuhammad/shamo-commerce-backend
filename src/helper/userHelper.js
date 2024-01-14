5d68c0504d163d53ffc26df3015b5e1e3ad0497e
Rafid Muhammad <rafid.muhammad@gmail.com>
2024-01-14 16:43:42 +0700

    update user helper

--- src/helper/userHelper.js
+++ src/helper/userHelper.js
@@ -4,12 +4,23 @@ const bcrypt = require('bcrypt');
 const checkUserWithEmail = async (email) => {
     const userQuery = await User.query().where('email_address', email);
     const isExist = userQuery.length == 0 ? false : true;
-    return isExist;
+    return userQuery;
 }
 
 const hashPassword = async (password) => {
     const saltRounds = 10;
-    return await bcrypt.hash(password,saltRounds);
+    return await bcrypt.hash(password, saltRounds);
 }
 
-module.exports = {checkUserWithEmail, hashPassword};
+async function validateUser(req, res, schema) {
+    const { error, value } = schema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
+    req.validatedData = value;
+
+}
+
+module.exports = { checkUserWithEmail, hashPassword, validateUser };
